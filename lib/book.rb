f064d53050ed210726fd2e9d5214c77dbd3d10e9
Alexia Velador <alexiavel01@gmail.com>
2022-04-05 11:38:55 -0600

    commit tree b7fdf7a316d25a1bb0ab986a7cdb191d25d4416e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-
-
+class Book
+    attr_reader :name
+    attr_accessor :page_count
+    def initialize(title)
+        @title = title
+    end
+    def properties(title, author, page_count, genre)
+        title = title
+        author = author
+        page_count = page_count
+        genre = genre
+    end
+end
